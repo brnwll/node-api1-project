7f5f15918b73e941e4bd64147ded7e07eb0adfeb
Brian Howell <brianhowell@pm.me>
2024-02-26 16:17:04 -0500

    Remove comment

--- index.js
+++ index.js
@@ -6,17 +6,3 @@ const port = 9000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
-
-/*
-
-EXAMPLE PROJECT
-
-const server = require('./api/server')
-const PORT = 9000
-
-server.listen(PORT, () => {
-  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`)
-})
-
-
-*/
