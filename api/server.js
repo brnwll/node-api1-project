743f74948c19018009ce5d1863d2c03067a608d5
Brian Howell <brianhowell@pm.me>
2024-02-26 16:16:35 -0500

    mvp server implementation

--- api/server.js
+++ api/server.js
@@ -1,3 +1,75 @@
-// BUILD YOUR SERVER HERE
+const express = require("express");
+const User = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json());
+
+// GET /api/users
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// GET /api/users/:id
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then((user) => {
+      user
+        ? res.status(200).json(user)
+        : res.status(404).json({ message: `no user with id ${id}` });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// POST /api/users
+server.post("/api/users", async (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({ message: "name and bio are required" });
+  } else {
+    //
+    const newUser = await User.insert(user);
+    if (newUser) {
+      res.status(201).json(newUser);
+    } else {
+      res.status(500).json({ message: "error adding user" });
+    }
+  }
+});
+
+// PUT /api/users/:id
+server.put("/api/users/:id", async (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+  if (!changes.name || !changes.bio) {
+    res.status(400).json({ message: "name and bio are required" });
+  } else {
+    const updatedUser = await User.update(id, changes);
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: `no user with id ${id}` });
+    }
+  }
+});
+
+// DELETE /api/users/:id
+server.delete("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedUser = await User.remove(id);
+  if (deletedUser) {
+    res.status(200).json(deletedUser);
+  } else {
+    res.status(404).json({ message: `no user with id ${id}` });
+  }
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
+
+/*
+
+EXAMPLE PROJECT
+
+const server = require('./api/server')
+const PORT = 9000
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`)
+})
+
+
+*/
